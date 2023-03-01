63bf5fef28d4325bb3776f332ccd860c693b31d8
zer0silver <duddms0_0@naver.com>
2023-03-02 00:27:45 +0900

    init: [#1] spring boot 세팅 & 데이터베이스 연결 - 개발 시 h2 database를 사용합니다.

---
+++ src/main/java/com/ooogaoooga/OoogaOoogaApplication.java
@@ -0,0 +1,13 @@
+package com.ooogaoooga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OoogaOoogaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OoogaOoogaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ooogaoooga/OoogaOoogaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ooogaoooga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OoogaOoogaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
