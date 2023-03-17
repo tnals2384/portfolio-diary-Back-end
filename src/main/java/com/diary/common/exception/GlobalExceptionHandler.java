9d2b328ead4c490214b01ae02af08eed0f45227c
zer0silver <duddms0_0@naver.com>
2023-03-17 21:09:28 +0900

    chore:[#1] ResponseEntityExceptionHandler를 상속

--- src/main/java/com/diary/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/diary/common/exception/GlobalExceptionHandler.java
@@ -5,10 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     /*
      * Developer Custom Exception: 직접 정의한 RestApiException 에러 클래스에 대한 예외 처리
