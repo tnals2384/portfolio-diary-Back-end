fc71b72730d815c2ce5a438e3c2fdd709392d59d
tnals2384 <tnals2384@gmail.com>
2023-03-20 20:18:09 +0900

    fix: [#3] repository generic type 수정 - JPArepository generic type 수정

--- src/main/java/com/diary/domain/file/repository/FileRepository.java
+++ src/main/java/com/diary/domain/file/repository/FileRepository.java
@@ -1,7 +1,8 @@
 package com.diary.domain.file.repository;
 
+import com.diary.domain.file.model.File;
 import com.diary.domain.member.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FileRepository extends JpaRepository<Member,Long>, FileRepositoryCustom {
+public interface FileRepository extends JpaRepository<File,Long>, FileRepositoryCustom {
 }
--- src/main/java/com/diary/domain/tag/repository/TagRepository.java
+++ src/main/java/com/diary/domain/tag/repository/TagRepository.java
@@ -1,7 +1,8 @@
 package com.diary.domain.tag.repository;
 
 import com.diary.domain.member.model.Member;
+import com.diary.domain.tag.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TagRepository extends JpaRepository<Member,Long>, TagRepositoryCustom {
+public interface TagRepository extends JpaRepository<Tag,Long>, TagRepositoryCustom {
 }
