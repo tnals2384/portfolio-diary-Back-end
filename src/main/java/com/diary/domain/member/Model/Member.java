d0213fcdfb23413ce9d339199fc3ff34e02edba8
zer0silver <duddms0_0@naver.com>
2023-04-28 15:53:08 +0900

    feat:[#2] 엔티티 수정

--- src/main/java/com/diary/domain/member/Model/Member.java
+++ src/main/java/com/diary/domain/member/Model/Member.java
@@ -2,6 +2,7 @@ package com.diary.domain.member.model;
 
 import com.diary.common.base.BaseEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,9 +17,6 @@ public class Member extends BaseEntity {
     @Column(name = "member_id")
     private Long id;
 
-    @Column(nullable = false)
-    private String name;
-
     private String nickname;
 
     @Column(nullable = false, unique = true)
@@ -30,8 +28,45 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private MemberRole role;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "provider_type")
-    private ProviderType providerType;
+    private String provider;
+
+    @Column(name = "provider_id")
+    private String providerId;
+
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Member(String nickname, String email, String profileImageUrl, String provider, String providerId){
+        this.nickname = nickname;
+        this.email = email;
+        this.profileImageUrl = profileImageUrl;
+        this.role = MemberRole.USER;
+        this.provider = provider;
+        this.providerId = providerId;
+    }
+
+    public static Member of(String nickname, String email, String profileImageUrl, String provider, String providerId){
+        return Member.builder()
+                .nickname(nickname)
+                .email(email)
+                .profileImageUrl(profileImageUrl)
+                .provider(provider)
+                .providerId(providerId)
+                .build();
+    }
+
+    public Member update(String nickname, String profileImageUrl){
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+
+        return this;
+    }
+
+    public Member updateProvider(String provider){
+        this.provider = provider;
+
+        return this;
+    }
+
 
 }
