3c7690abbf18e1644282fe24aad698770d6f72c0
zer0silver <duddms0_0@naver.com>
2023-04-28 15:54:26 +0900

    feat:[#2] 구글 로그인 기능 추가 - 테스트 필요

--- src/main/java/com/diary/domain/post/repository/PostRepository.java
+++ src/main/java/com/diary/domain/post/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.diary.domain.post.repository;
 
-import com.diary.domain.member.model.Member;
+import com.diary.domain.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Member,Long>, PostRepositoryCustom {
+public interface PostRepository extends JpaRepository<Post,Long>, PostRepositoryCustom {
 }
