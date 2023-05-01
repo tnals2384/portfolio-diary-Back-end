cd7d3b690187de869386fde8337a88aa0ed17ff5
tnals2384 <tnals2384@gmail.com>
2023-05-01 18:40:12 +0900

    fix:[#12] 병합과정 오류 수정 -커밋 오류

--- src/main/java/com/diary/domain/post/repository/PostRepository.java
+++ src/main/java/com/diary/domain/post/repository/PostRepository.java
@@ -1,10 +1,8 @@
 package com.diary.domain.post.repository;
 
-<<<<<<< HEAD
-import com.diary.domain.experience.model.Experience;
+
 import com.diary.domain.member.model.Member;
-=======
->>>>>>> upstream/dev
+
 import com.diary.domain.post.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
