6132015b5bc71bc98bdefd4a5d87c856a97fce74
tnals2384 <tnals2384@gmail.com>
2023-05-04 21:15:22 +0900

    refactor: [#22] memberUtil 사용하도록 변경 - OAuthLoginMemberUtil을 사용하여 로그인한 멤버정보를 받아와 Post CRUD 실행하도록 리팩토링

--- src/main/java/com/diary/domain/post/controller/PostController.java
+++ src/main/java/com/diary/domain/post/controller/PostController.java
@@ -1,10 +1,11 @@
 package com.diary.domain.post.controller;
 
 import com.diary.common.base.BaseResponse;
+import com.diary.domain.auth.OAuthLoginMemberUtil;
+import com.diary.domain.member.model.Member;
 import com.diary.domain.post.model.dto.*;
 import com.diary.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -19,51 +20,48 @@ import java.util.List;
 @RestController
 public class PostController {
     private final PostService postService;
+    private final OAuthLoginMemberUtil oAuthLoginMemberUtil;
 
     @PostMapping("/api/posts")
     public BaseResponse<CreatePostResponse> createPost(
-            @RequestParam @Valid Long memberId,
             @RequestPart @Valid CreatePostRequest createPostRequest,
             @RequestPart(value = "file", required = false) List<MultipartFile> files) throws IOException {
-
-        return new BaseResponse<>(postService.createPost(memberId, createPostRequest, files));
+        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+        return new BaseResponse<>(postService.createPost(loginMember, createPostRequest, files));
     }
 
     @PutMapping("/api/posts/{postId}")
     public BaseResponse<UpdatePostResponse> updatePost(
-            @RequestParam @Valid Long memberId,
             @PathVariable @Valid Long postId,
             @RequestPart @Valid UpdatePostRequest updatePostRequest,
-            @RequestPart(value="file", required = false) List<MultipartFile> files) throws IOException {
-        return new BaseResponse<>(postService.updatePost(memberId,postId,updatePostRequest,files));
+            @RequestPart(value = "file", required = false) List<MultipartFile> files) throws IOException {
+        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+        return new BaseResponse<>(postService.updatePost(loginMember, postId, updatePostRequest, files));
     }
 
 
     @DeleteMapping("/api/posts/{postId}")
-    public BaseResponse<DeletePostResponse> deletePost(@RequestParam @Valid Long memberId,
-                                                       @PathVariable @Valid Long postId){
-        return new BaseResponse<>(postService.deletePost(memberId,postId));
+    public BaseResponse<DeletePostResponse> deletePost(@PathVariable @Valid Long postId) {
+        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+        return new BaseResponse<>(postService.deletePost(loginMember, postId));
     }
 
     //상세 조회
     @GetMapping("/api/posts/{postId}")
     public BaseResponse<GetPostResponse> getPost(
-            @RequestParam @Valid Long memberId,
-            @PathVariable @Valid Long postId
-    ) {
-        return new BaseResponse<>(postService.getPost(memberId, postId));
+            @PathVariable @Valid Long postId) {
+        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+        return new BaseResponse<>(postService.getPost(loginMember, postId));
     }
 
     //Post 목록 조회 with Paging
     @GetMapping("/api/posts")
     public BaseResponse<GetPagePostsResponse> getAllPosts(
-            @RequestParam @Valid Long memberId,
             @RequestParam(defaultValue = "id", value = "orderBy") String orderType,
-            @PageableDefault(size= 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-
-        return new BaseResponse<>(postService.getAllPostsWithPaging(memberId,orderType,pageable));
+            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+        return new BaseResponse<>(postService.getAllPostsWithPaging(loginMember, orderType, pageable));
     }
-
 
 
 }
