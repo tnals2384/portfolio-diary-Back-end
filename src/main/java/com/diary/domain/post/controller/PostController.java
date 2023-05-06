36574d473fdc5c832d39f71d7a43dd3fe91231ce
zer0silver <duddms0_0@naver.com>
2023-05-06 23:33:50 +0900

    feat: [#18] 로그인한 유저 가져오는 util 보완 필요 - 현재 포스트맨에서 테스트 불가능

--- src/main/java/com/diary/domain/post/controller/PostController.java
+++ src/main/java/com/diary/domain/post/controller/PostController.java
@@ -1,8 +1,10 @@
 package com.diary.domain.post.controller;
 
 import com.diary.common.base.BaseResponse;
-import com.diary.domain.auth.OAuthLoginMemberUtil;
+import com.diary.common.exception.ErrorCode;
+import com.diary.common.exception.RestApiException;
 import com.diary.domain.member.model.Member;
+import com.diary.domain.member.repository.MemberRepository;
 import com.diary.domain.post.model.dto.*;
 import com.diary.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -20,48 +22,56 @@ import java.util.List;
 @RestController
 public class PostController {
     private final PostService postService;
-    private final OAuthLoginMemberUtil oAuthLoginMemberUtil;
+    private final MemberRepository memberRepository;
 
     @PostMapping("/api/posts")
     public BaseResponse<CreatePostResponse> createPost(
+            @RequestParam @Valid Long memberId,
             @RequestPart @Valid CreatePostRequest createPostRequest,
             @RequestPart(value = "file", required = false) List<MultipartFile> files) throws IOException {
-        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
         return new BaseResponse<>(postService.createPost(loginMember, createPostRequest, files));
     }
 
     @PutMapping("/api/posts/{postId}")
     public BaseResponse<UpdatePostResponse> updatePost(
+            @RequestParam @Valid Long memberId,
             @PathVariable @Valid Long postId,
             @RequestPart @Valid UpdatePostRequest updatePostRequest,
             @RequestPart(value = "file", required = false) List<MultipartFile> files) throws IOException {
-        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
         return new BaseResponse<>(postService.updatePost(loginMember, postId, updatePostRequest, files));
     }
 
 
     @DeleteMapping("/api/posts/{postId}")
-    public BaseResponse<DeletePostResponse> deletePost(@PathVariable @Valid Long postId) {
-        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+    public BaseResponse<DeletePostResponse> deletePost(@RequestParam @Valid Long memberId, @PathVariable @Valid Long postId) {
+
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
         return new BaseResponse<>(postService.deletePost(loginMember, postId));
     }
 
     //상세 조회
     @GetMapping("/api/posts/{postId}")
     public BaseResponse<GetPostResponse> getPost(
+            @RequestParam @Valid Long memberId,
             @PathVariable @Valid Long postId) {
-        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
         return new BaseResponse<>(postService.getPost(loginMember, postId));
     }
 
     //Post 목록 조회 with Paging
     @GetMapping("/api/posts")
     public BaseResponse<GetPagePostsResponse> getAllPosts(
+            @RequestParam @Valid Long memberId,
             @RequestParam(defaultValue = "id", value = "orderBy") String orderType,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
-        Member loginMember = oAuthLoginMemberUtil.getGoogleLoginMember();
+
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
         return new BaseResponse<>(postService.getAllPostsWithPaging(loginMember, orderType, pageable));
     }
-
 
 }
