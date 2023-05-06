407f6734ece1f56792bf803d5627b2ca4d173ac2
zer0silver <duddms0_0@naver.com>
2023-05-06 23:32:38 +0900

    feat: [#18] OAuth2User를 사용하도록 수정

--- src/main/java/com/diary/domain/auth/OAuthLoginMemberUtil.java
+++ src/main/java/com/diary/domain/auth/OAuthLoginMemberUtil.java
@@ -7,8 +7,8 @@ import com.diary.domain.member.model.Member;
 import com.diary.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 
 
@@ -19,9 +19,9 @@ public class OAuthLoginMemberUtil {
 
     private final MemberRepository memberRepository;
 
-    public Member getGoogleLoginMember() {
-        OAuth2AuthenticationToken authentication = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
-        String email = authentication.getPrincipal().getAttributes().get("email").toString();
+    public Member getGoogleLoginMember(Authentication authentication) {
+        OAuth2User principal = (OAuth2User) authentication.getPrincipal();
+        String email = principal.getAttributes().get("email").toString();
 
         return memberRepository.findByEmail(email).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
     }
