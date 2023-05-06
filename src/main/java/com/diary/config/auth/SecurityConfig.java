57c680741f604b813e93a25e60e939b79582fb89
zer0silver <duddms0_0@naver.com>
2023-05-06 23:34:13 +0900

    feat: [#18] SecurityConfig 수정

--- src/main/java/com/diary/config/auth/SecurityConfig.java
+++ src/main/java/com/diary/config/auth/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.diary.config.auth;
 import com.diary.domain.auth.OAuthService;
 import com.diary.domain.member.model.MemberRole;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class SecurityConfig {
 
     private final OAuthService oAuthService;
@@ -22,20 +24,18 @@ public class SecurityConfig {
                 //CORS 설정
                 .cors()
                 .and()
-                //Cross-Site-Request-Forgery 웹 브라우저가 신뢰할 수 없는 악성 사이트에서 사용자가 원치않는 작업을 수행하는 공격
-                //쿠키에 의존하지 않고 OAuth2.0, JWT를 사용하는 REST API의 경우 CSRF 보호가 필요하지 않음
                 .csrf().disable()
-                //basic 인증방식은 username:password를 base64 인코딩으로 Authroization 헤더로 보내는 방식
                 .httpBasic().disable()
                 .formLogin().disable()
                 //요청에 대한 인가 처리 설정
                 .authorizeRequests()
                 .antMatchers("/","/oauth2/**").permitAll() // 로그인은 누구나 가능하도록
                 .antMatchers("/api/v1/**").hasRole(MemberRole.USER.name()) // 유저만 접속 가능
-                .anyRequest().authenticated() // 그 외엔 모두 인증 필요
+                .anyRequest().permitAll() // 개발 완료 전까지 모두 접속 가능하도록
                 .and()
                 //OAuth 2.0 기반 인증을 처리하기위해 Provider와의 연동을 지원
                 .oauth2Login()
+                .defaultSuccessUrl("/")
                 .userInfoEndpoint()
                 //OAuth 2.0 인증이 처리되는데 사용될 사용자 서비스를 지정하는 메서드
                 .userService(oAuthService);
