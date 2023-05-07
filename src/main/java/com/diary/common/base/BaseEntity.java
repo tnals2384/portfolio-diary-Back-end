aea5442c8977a75532a86df2126d3b5da8a96276
tnals2384 <tnals2384@gmail.com>
2023-05-07 10:20:37 +0900

    feat: [#26] status 변경함수 추가 - Base Entity에 status를 변경하는 함수를 추가

--- src/main/java/com/diary/common/base/BaseEntity.java
+++ src/main/java/com/diary/common/base/BaseEntity.java
@@ -25,10 +25,23 @@ public class BaseEntity {
     @Enumerated(EnumType.STRING)
     private BaseStatus status = BaseStatus.ACTIVE;
 
+
     /**
      * 수정 시간 업데이트
      */
     public void updateUpdatedAt(@NotNull LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
     }
+
+    /**
+     * Status 변경
+     * @param status
+     */
+    public void changeStatus(BaseStatus status) {
+        if (status.equals(BaseStatus.ACTIVE))
+            this.status = BaseStatus.INACTIVE;
+        else
+            this.status=BaseStatus.ACTIVE;
+    }
+
 }
