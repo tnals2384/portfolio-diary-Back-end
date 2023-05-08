9f14bc6f682fcb45c0de2453bc3ad2d71d028d6a
zer0silver <duddms0_0@naver.com>
2023-05-09 00:54:51 +0900

    [feat]: [#28] webconfig 설정 추가

---
+++ src/main/java/com/diary/config/web/WebConfig.java
@@ -0,0 +1,20 @@
+package com.diary.config.web;
+
+import com.diary.config.auth.LoginMemberArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginMemberArgumentResolver);
+    }
+}
