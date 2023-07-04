b4f02fdde9de606915132e157f89d69dd83e2a23
tnals2384 <tnals2384@gmail.com>
2023-07-04 22:50:02 +0900

    fix : [#51] NoArgsConstructor 어노테이션 추가

--- src/main/java/com/diary/domain/tag/model/dto/CreateTagRequest.java
+++ src/main/java/com/diary/domain/tag/model/dto/CreateTagRequest.java
@@ -3,10 +3,12 @@ package com.diary.domain.tag.model.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateTagRequest {
     private String tagType;
--- src/main/java/com/diary/domain/tag/model/dto/CreateTagResponse.java
+++ src/main/java/com/diary/domain/tag/model/dto/CreateTagResponse.java
@@ -4,10 +4,12 @@ package com.diary.domain.tag.model.dto;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateTagResponse {
     private List<Long> tags;
