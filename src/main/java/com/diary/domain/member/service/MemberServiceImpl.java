db62b98500ba520e7eefb75497b9ce4f3e348ebf
zer0silver <duddms0_0@naver.com>
2023-07-18 12:54:58 +0900

    feat: [#53] 마이페이지에서 필요한 유저 정보 조회 API

--- src/main/java/com/diary/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/diary/domain/member/service/MemberServiceImpl.java
@@ -1,11 +1,27 @@
 package com.diary.domain.member.service;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.diary.common.exception.ErrorCode;
+import com.diary.common.exception.RestApiException;
+import com.diary.domain.member.model.Member;
+import com.diary.domain.member.model.dto.FindMyPageUserResponse;
+import com.diary.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
-    private final JPAQueryFactory queryFactory;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public FindMyPageUserResponse findMyPageUser(Long memberId) {
+        Member loginMember = memberRepository.findById(memberId).orElseThrow(() -> new RestApiException(ErrorCode.NO_LOGIN_USER));
+        String nickname = loginMember.getNickname();
+        Long daysSinceSignUp = ChronoUnit.DAYS.between(loginMember.getCreatedAt().toLocalDate(), LocalDate.now());
+
+        return new FindMyPageUserResponse(nickname, daysSinceSignUp);
+    }
 }
